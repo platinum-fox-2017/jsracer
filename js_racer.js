41105a30ea03654392b30d572274fe2e29c045ae
ryanponti <ryanakbar.armial@gmail.com>
2018-02-08 10:24:14 +0700

    Template

---
+++ js_racer.js
@@ -0,0 +1,29 @@
+"use strict"
+
+const Dice = require('./dice');
+
+class JSRacer {
+  constructor(players, length) {
+
+  }
+  print_board() {
+
+  }
+  print_line(player, pos) {
+
+  }
+  advanced_player(player) {
+
+  }
+  finished() {
+
+  }
+  winner() {
+
+  }
+  reset_board() {
+    console.log("\x1B[2J")
+  }
+}
+
+module.exports = JSRacer;
