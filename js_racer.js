7ca0c8a055493a6e393b730ccb57e5bf0df7ef56
root <irsyadpahlapi28@gmail.com>
2018-02-08 04:24:45 -0500

    upload 3 file dice racer and runner

--- js_racer.js
+++ js_racer.js
@@ -3,23 +3,62 @@
 const Dice = require('./dice');
 
 class JSRacer {
-  constructor(players, length) {
-
+  constructor(players, length,pos) {
+    this.player=players
+    this.rute=length
+    this.pos=pos
+    this.dices=new Dice()
+    this.win=''
   }
   print_board() {
-
+    for(let i=0;i<this.player.length;i++){
+      this.print_line(this.player[i],this.pos[i])
+    }
   }
-  print_line(player, pos) {
 
+  print_line(player,pos) {
+    var board=[]
+    for(let i=0;i<this.rute;i++){
+      board.push(' ')
+    }
+    board[pos]=player
+    board=board.join('|')
+    console.log(board)
   }
-  advanced_player(player) {
 
+  sleep() {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > 1000) {
+        break;
+      }
+    }
   }
+  move(player) {
+    var hasil=0
+    while(hasil<this.rute-1){
+      this.reset_board()
+      for(let i=0;i<player.length;i++){
+        hasil=this.pos[i]+this.dices.roll()
+        if(hasil>=this.rute-1){
+          this.pos[i]=this.rute-1
+          this.win=player[i]
+          break;
+        }else{
+          this.pos[i]=hasil
+        }
+      }
+      this.print_board()
+      console.log()
+      this.sleep()
+    }
+  }
+
   finished() {
 
   }
   winner() {
-
+    console.log(`selamat ${this.win} menang`)
   }
   reset_board() {
     console.log("\x1B[2J")
--- runner.js
+++ runner.js
@@ -2,13 +2,28 @@
 
 const JSRacer = require('./js_racer')
 
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds) {
-      break;
-    }
+function racer(jumlahpemain,panjangrute){
+  var hurufmobil= 'abcdefghijklmnopqrstuvwxyz'
+  var namemobil=[]
+  var pos=[]
+  for(let i=0;i<jumlahpemain;i++){
+    namemobil.push(hurufmobil[i])
+    pos.push(0)
   }
-}
+  var my_jsracer= new JSRacer(namemobil,panjangrute,pos)
+  my_jsracer.print_board()
+  console.log()
+  my_jsracer.move(my_jsracer.player)
+  my_jsracer.winner()
 
+}
+var myargv=process.argv
+racer(myargv[2],myargv[3])
+
+
+
+// my_jsracer.print_board()
+// my_jsracer.print_line(my_jsracer.nameplayer,0)
+// my_jsracer.advanced_player(my_jsracer.player)
+// console.log(my_jsracer)
 // Your code here...
