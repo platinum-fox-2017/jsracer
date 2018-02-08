69bc0b14f27525583552dc978a0cf2c44ca9a45f
Reynaldi Ananda Pane <reynaldipane@gmail.com>
2018-02-08 17:04:56 +0700

    Release 2 : Add Trap in track

--- js_racer.js
+++ js_racer.js
@@ -8,6 +8,7 @@ class JSRacer {
     this.players      = players;
     this.length       = length;
     this.arrPlayer    = [];
+    this.arrTrap      = [];
   }
 
   generateGame() {
@@ -23,6 +24,14 @@ class JSRacer {
     }
   }
 
+  generateTrap() {
+      var indexTrap = Math.floor(Math.random() * this.length-1) + 1;
+      while(indexTrap == 0 || indexTrap == this.length-1) {
+        indexTrap = Math.floor(Math.random() * this.length-1) + 1;
+      }
+      this.arrTrap.push(indexTrap);
+  }
+
   print_board() {
     for (let i = 0; i < this.arrPlayer.length; i++) {
       console.log(this.print_line(this.arrPlayer[i].nama, this.arrPlayer[i].position));
@@ -32,9 +41,15 @@ class JSRacer {
 
   print_line(playerName, pos) {
     var track = [];
+    var idxTrap = 0;
     for (let i = 0; i < this.length; i++) {
       if(i == pos) {
         track.push(playerName);  
+      } else if (i == this.arrTrap[idxTrap] ) {
+        track.push('x');
+        if (idxTrap < this.arrTrap.length) {
+          idxTrap = idxTrap + 1;
+        }
       } else {
         track.push('_');        
       }
@@ -51,16 +66,15 @@ class JSRacer {
       player.position = this.length-1;
     } else {
       player.position = numberOfMove;
+      if (player.position == this.arrTrap[0]) {
+        player.position = 0;
+        console.log(player.nama + " kena jebakan !");
+      }
     }
   }
 
   finished() {
-    for(let i = 0; i < this.arrPlayer.length; i++) {
-      if ((this.arrPlayer[i].position+1) == this.length) {
-        return this.winner();
-      }
-    }
-    return false;
+    return this.winner();
   }
 
   getPlayer() {
@@ -71,8 +85,10 @@ class JSRacer {
     for(let i = 0; i < this.arrPlayer.length; i++) {
       if ((this.arrPlayer[i].position+1) == this.length) {
         console.log("Player " + this.arrPlayer[i].nama + " win !");
+        return true;
       }
     }
+    return false;
   }
 
   reset_board() {
