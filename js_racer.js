6aff449048d085806e2235b2aaff6a5a42f19b7c
Ardhiansyah Putra <ardhimetanaputra@gmail.com>
2018-02-08 11:42:13 +0700

    'Release'

--- js_racer.js
+++ js_racer.js
@@ -4,22 +4,49 @@ const Dice = require('./dice');
 
 class JSRacer {
   constructor(players, length) {
-
+    this.players = players;
+    this.length = length;
   }
   print_board() {
-
+    for (var i = 0; i < this.players.length; i++) {
+      this.print_line(this.players[i].name, this.players[i].position);
+    }
   }
   print_line(player, pos) {
+    var board = [];
+    for (var j = 0; j < this.length; j++) {
+      if (pos == j) board.push(player);
+      else board.push(' ');
 
+      board.push('|');
+    }
+    console.log(board.join(''));
   }
-  advanced_player(player) {
+  move(player) {
+    let dice = new Dice().roll();
 
+    for (var i = 0; i < this.players.length; i++) {
+      if (this.players[i].name == player) {
+        if (this.players[i].position + dice > this.length - 1) {
+          this.players[i].position = this.length - 1;
+        } else this.players[i].position += dice;
+      }
+    }
   }
   finished() {
+    for (var i = 0; i < this.players.length; i++) {
+      if (this.players[i].position == this.length - 1) return true;
+    }
 
+    return false;
   }
   winner() {
-
+    for (var i = 0; i < this.players.length; i++) {
+      if (this.players[i].position == this.length - 1) {
+        console.log(`Player '${this.players[i].name}' is the winner`);
+        break;
+      }
+    }
   }
   reset_board() {
     console.log("\x1B[2J")
--- runner.js
+++ runner.js
@@ -11,4 +11,36 @@ function sleep(milliseconds) {
   }
 }
 
+function getPlayer(number) {
+	var result = [];
+	for (var i = 0; i < number; i++) {
+		var obj = {
+			name: String.fromCharCode(97 + i),
+			position: 0
+		};
+		result.push(obj);
+	}
+	return result;
+}
+
 // Your code here...
+var input_argv = process.argv;
+
+if (input_argv[2] < 2) console.log('Jumlah minimal pemain adalah 2!');
+else if (input_argv[3] < 15) console.log('Panjang lintasan paling sedikit adalah 15!');
+else {
+	var players = getPlayer(input_argv[2]);
+	var jsRacer = new JSRacer(players, input_argv[3]);
+
+	while(!jsRacer.finished()) {
+		jsRacer.reset_board();
+		for (var i = 0; i < players.length; i++) {
+			jsRacer.move(players[i].name);
+			if (jsRacer.finished()) break;
+		}
+		jsRacer.print_board();
+		sleep(2000);
+	}
+
+	jsRacer.winner();
+}
