ecee9ff0ab3c2c80338d83f1fa1444f3dbffcd83
Fitrul Islam <fitrul.islam@gmail.com>
2018-02-08 22:58:52 +0700

    belum sempurna

--- js_racer.js
+++ js_racer.js
@@ -4,26 +4,88 @@ const Dice = require('./dice');
 
 class JSRacer {
   constructor(players, length) {
-
+    this.player = players;
+    this.length = length;
+    this.playerInfo = this.infoRacers();
+    this.result = this.print_board();
+    this.finish = false;
+    this.win = '';
   }
+
   print_board() {
-
+    for(let s=0; s<this.player; s++){
+      if(this.playerInfo[s].position >= this.length-1){
+        this.print_line(this.playerInfo[s].name, this.length-1);
+        this.finish = true;
+      } else {
+        this.print_line(this.playerInfo[s].name, this.playerInfo[s].position);
+        var random = this.advanced_player();
+        this.playerInfo[s].position += random;
+      }
+    }
   }
+
+  infoRacers(){
+    let abjad = 'abcdefghijklmnopqrstuvqxyz'
+    var racers = []
+    for(let j=0; j<this.player; j++){
+      let racer = {
+        name: abjad[j],
+        position: 0
+      }
+      racers.push(racer);
+    }
+    return racers;
+  }
+
   print_line(player, pos) {
-
+    let line = [];
+    for(let i=0; i<this.length; i++){
+      if(i===pos){
+        line.push(player);
+      } else {
+        line.push(' ');
+      }
+    }
+    console.log(line.join('|'));
   }
+
   advanced_player(player) {
-
+    let random = Math.floor((Math.random()*6)+1);
+    return random;
   }
+
   finished() {
-
+    for(let c=0; c<this.player; c++){
+      if(this.playerInfo[c].position===this.length-1){
+        this.win = this.playerInfo[c].name;
+        this.winner();
+        this.finish = true;
+      }
+    }
   }
+
   winner() {
-
+    console.log('The winner is player '+ this.win);
   }
+
   reset_board() {
     console.log("\x1B[2J")
   }
 }
 
+
 module.exports = JSRacer;
+
+
+  // print_board(players) {
+  //   for(let i=0; i<this.numberOfPlayers; i++){
+  //     if(this.players[i].position >= this.length-1){
+  //       this.print_line(this.players[i].playerName, this.players[i].position);
+  //       this.finish = true;
+  //     }else{
+  //       this.print_line(this.players[i].playerName, this.players[i].position);
+  //       this.move_point(this.players[i]);
+  //     }
+  //   }
+  // }
--- runner.js
+++ runner.js
@@ -12,3 +12,13 @@ function sleep(milliseconds) {
 }
 
 // Your code here...
+var myArgv = process.argv;
+var play = new JSRacer(myArgv[2], myArgv[3]);
+play.infoRacers();
+
+while(!play.finish){
+  sleep(1000);
+  play.reset_board();
+  play.finished();
+  play.print_board();
+}
